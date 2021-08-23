821bc83641470b250dea691f5f851ddb5ead0dfa
briansandiford <rooms21@gmail.com>
2021-08-22 21:27:51 -0400

    added demo-selenium-test.sh

---
+++ demo-selenium-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo '### create python 3 virtual enviroment ###'
+
+VIRTUAL_ENV_NAME='virtual-enviroment'
+python3 -m venv $VIRTUAL_ENV_NAME
+
+echo '#### Activate virtual enviroment ###"
+source $VIRTUAL_ENV_NAME/bin/activate
+
+echo '#### Install requirements ###'
+pip3 install -r ./requirements.txt
+
+### Jump to the Path ####
+cd /var/lib/jenkins/workspace/$JOB_NAME/
+
+echo '##### Run Test #####'
+# pip3 install Flask
+python3 manage.py test
+
+echo '#### deactivate virtual environment ###'
+deactivate 
+exit
